54228df4ef08b5ef9019c49ad4fc0c6a2cca3334
exelimpichment <oldestspy@gmail.com>
2023-04-14 02:59:43 +0200

    api link added for test

--- src/SignupForm.tsx
+++ src/SignupForm.tsx
@@ -1,29 +1,51 @@
 import React from 'react';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 
+const url =
+  'https://first-node-js-express-project.onrender.com/api/v1/auth/register';
+
 const SignupForm = () => {
   return (
     <Formik
-      initialValues={{ firstName: '', lastName: '', email: '' }}
+      initialValues={{ name: '', email: '', password: '' }}
       onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
+        console.log(values);
+        fetch(url, {
+          method: 'POST',
+          credentials: 'include',
+          mode: 'cors',
+          headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+          },
+          body: JSON.stringify(values),
+        })
+          .then((resp) => {
+            resp.headers.forEach((val, key) => {
+              console.log(key, val);
+            });
+            let cookie = resp.headers.get('set-cookie');
+            console.log('set-cookie header value', cookie);
+
+            return resp.json();
+          })
+          .then((user) => console.log(user))
+
+          .catch((error) => console.warn(error));
+        setSubmitting(false);
       }}
     >
       <Form>
         <label htmlFor='firstName'>First Name</label>
-        <Field name='firstName' type='text' />
+        <Field name='name' type='text' />
         <ErrorMessage name='firstName' />
-
-        <label htmlFor='lastName'>Last Name</label>
-        <Field name='lastName' type='text' />
-        <ErrorMessage name='lastName' />
 
         <label htmlFor='email'>Email Address</label>
         <Field name='email' type='email' />
         <ErrorMessage name='email' />
+
+        <label htmlFor='lastName'>Last Name</label>
+        <Field name='password' type='text' />
+        <ErrorMessage name='lastName' />
 
         <button type='submit'>Submit</button>
       </Form>
